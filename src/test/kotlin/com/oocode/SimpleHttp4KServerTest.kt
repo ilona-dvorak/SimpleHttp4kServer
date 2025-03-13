381bfeb722d230474ea3c4b910e9fd762725060f
Ivan Moore <ivan@teamoptimization.com>
2025-03-13 14:46:53 +0000

    removed unnecessary comment

--- src/test/kotlin/com/oocode/SimpleHttp4KServerTest.kt
+++ src/test/kotlin/com/oocode/SimpleHttp4KServerTest.kt
@@ -3,18 +3,14 @@ package com.oocode
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.containsSubstring
 import com.natpryce.hamkrest.equalTo
-import org.http4k.client.JavaHttpClient
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.hamkrest.hasBody
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 class SimpleHttp4KServerTest {
-    // This is how you'd typically test http4k services
     @Test
     fun canAnswerMyName() {
         assertThat(app(Request(GET, "?q=What+is+your+name%3F")), equalTo(Response(OK).body("Someone")))
