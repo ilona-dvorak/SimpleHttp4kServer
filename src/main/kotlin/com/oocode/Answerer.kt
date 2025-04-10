12a522d804e90db777af1aebb943027a8d54a30b
Ilona Dvorak <ape02@ox.ac.uk>
2025-04-10 15:40:24 +0100

    10

--- src/main/kotlin/com/oocode/Answerer.kt
+++ src/main/kotlin/com/oocode/Answerer.kt
@@ -17,29 +17,16 @@ class Answerer {
             return numbers.maxOrNull().toString();
 
         }
-        else if (question.contains("Which of the following numbers is the largest: 4, 34, 27?")) {
-            return "34"
-        }
-        else if (question.contains("Which of the following numbers is the largest: 33, 7, 92?")) {
-            return "92"
-        }
-        else if (question.contains("What is 31 plus 4?")) {
-            return "35"
-        }
-        else if (question.contains("What is 16 plus 5?")) {
-            return "21"
-        }
-        else if (question.contains("What is 85 plus 7?")) {
-            return "92"
-        }
-        else if (question.contains("Which of the following numbers is the largest: 31, 60, 67?")) {
-            return "67"
-        }
-        else if (question.contains("What is 97 plus 57?")) {
-            return "154"
-        }
-        else if (question.contains("What is 26 plus 28?")) {
-            return "54"
+        if (question.contains("plus")) {
+            val regex = "\\d+".toRegex()
+
+            // Find all matches of the regex in the input string
+            val numbers = regex.findAll(question)
+                .map { it.value.toInt() }  // Convert each match (String) to an Integer
+                .toList()
+
+            // Return the largest number, or null if no numbers were found
+            return numbers.sum().toString();
         }
         else if (question.contains("2")) {
             return "4"
