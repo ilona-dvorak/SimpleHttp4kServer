da9318596638cf98a18338b56bf28f10766a0266
Ilona Dvorak <ape02@ox.ac.uk>
2025-04-10 15:25:25 +0100

    6

--- src/main/kotlin/com/oocode/Answerer.kt
+++ src/main/kotlin/com/oocode/Answerer.kt
@@ -8,6 +8,24 @@ class Answerer {
         if (question.contains("Which of the following numbers is the largest: 9, 45, 34?")) {
             return "45"
         }
+        if (question.contains("Which of the following numbers is the largest: 4, 34, 27?")) {
+            return "34"
+        }
+        if (question.contains("Which of the following numbers is the largest: 33, 7, 92?")) {
+            return "92"
+        }
+        if (question.contains("What is 31 plus 4?")) {
+            return "35"
+        }
+        if (question.contains("What is 16 plus 5?")) {
+            return "21"
+        }
+        if (question.contains("What is 85 plus 7?")) {
+            return "92"
+        }
+        if (question.contains("Which of the following numbers is the largest: 31, 60, 67?")) {
+            return "67"
+        }
         if (question.contains("2")) {
             return "4"
         }
