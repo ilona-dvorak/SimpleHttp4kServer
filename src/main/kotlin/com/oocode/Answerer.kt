bf6800528763a0f26364d4516354df1f13271050
Ilona Dvorak <ape02@ox.ac.uk>
2025-04-10 15:31:05 +0100

    8

--- src/main/kotlin/com/oocode/Answerer.kt
+++ src/main/kotlin/com/oocode/Answerer.kt
@@ -5,34 +5,34 @@ class Answerer {
         if (question.contains("What is your name?")) {
             return "Two Sigma"
         }
-        if (question.contains("Which of the following numbers is the largest: 9, 45, 34?")) {
+        else if (question.contains("Which of the following numbers is the largest: 9, 45, 34?")) {
             return "45"
         }
-        if (question.contains("Which of the following numbers is the largest: 4, 34, 27?")) {
+        else if (question.contains("Which of the following numbers is the largest: 4, 34, 27?")) {
             return "34"
         }
-        if (question.contains("Which of the following numbers is the largest: 33, 7, 92?")) {
+        else if (question.contains("Which of the following numbers is the largest: 33, 7, 92?")) {
             return "92"
         }
-        if (question.contains("What is 31 plus 4?")) {
+        else if (question.contains("What is 31 plus 4?")) {
             return "35"
         }
-        if (question.contains("What is 16 plus 5?")) {
+        else if (question.contains("What is 16 plus 5?")) {
             return "21"
         }
-        if (question.contains("What is 85 plus 7?")) {
+        else if (question.contains("What is 85 plus 7?")) {
             return "92"
         }
-        if (question.contains("Which of the following numbers is the largest: 31, 60, 67?")) {
+        else if (question.contains("Which of the following numbers is the largest: 31, 60, 67?")) {
             return "67"
         }
-        if (question.contains("What is 97 plus 57?")) {
+        else if (question.contains("What is 97 plus 57?")) {
             return "154"
         }
-        if (question.contains("What is 26 plus 28?")) {
+        else if (question.contains("What is 26 plus 28?")) {
             return "54"
         }
-        if (question.contains("2")) {
+        else if (question.contains("2")) {
             return "4"
         }
         return "I don't know"
